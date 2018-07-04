5681440109d690011e310878ba1f2e673ba46834
yuanjiaxin <yuanjiaxin@fmsh.com.cn>
2018-07-04 16:06:37 +0800

    初始化

---
+++ src/main/java/com/fmsh/blockchain/core/controller/RegisterController.java
@@ -0,0 +1,19 @@
+package com.fmsh.blockchain.core.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Author: yuanjiaxin
+ * @Date: 2018/7/4 14:48
+ * @Description:
+ */
+@Controller
+@RequestMapping("/register")
+public class RegisterController {
+
+    @RequestMapping("/")
+    public void register() {
+
+    }
+}
